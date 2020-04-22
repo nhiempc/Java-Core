f42a0edd519724d20ad4787509313047371a867c
Nguyễn Văn Nhiệm <62824306+nhiempc@users.noreply.github.com>
2020-04-22 18:36:08 +0700

    Add files via upload

---
+++ newspaper/Post.java
@@ -0,0 +1,97 @@
+package newspaper;
+
+public class Post {
+    private int postID;
+    private String postTitle;
+    private String postContent;
+    private String postSumary;
+    private String postImage;
+    private String postUploadDate;
+    private int postView;
+    private String postKeyword;
+    private int postCategoryID;
+
+    public void findByCategory(int categoryID){
+
+    }
+
+    public void viewDetail(int postID){
+
+    }
+
+    public void newest(){
+
+    }
+
+    public int getPostID() {
+        return postID;
+    }
+
+    public void setPostID(int postID) {
+        this.postID = postID;
+    }
+
+    public String getPostTitle() {
+        return postTitle;
+    }
+
+    public void setPostTitle(String postTitle) {
+        this.postTitle = postTitle;
+    }
+
+    public String getPostContent() {
+        return postContent;
+    }
+
+    public void setPostContent(String postContent) {
+        this.postContent = postContent;
+    }
+
+    public String getPostSumary() {
+        return postSumary;
+    }
+
+    public void setPostSumary(String postSumary) {
+        this.postSumary = postSumary;
+    }
+
+    public String getPostImage() {
+        return postImage;
+    }
+
+    public void setPostImage(String postImage) {
+        this.postImage = postImage;
+    }
+
+    public String getPostUploadDate() {
+        return postUploadDate;
+    }
+
+    public void setPostUploadDate(String postUploadDate) {
+        this.postUploadDate = postUploadDate;
+    }
+
+    public int getPostView() {
+        return postView;
+    }
+
+    public void setPostView(int postView) {
+        this.postView = postView;
+    }
+
+    public String getPostKeyword() {
+        return postKeyword;
+    }
+
+    public void setPostKeyword(String postKeyword) {
+        this.postKeyword = postKeyword;
+    }
+
+    public int getPostCategoryID() {
+        return postCategoryID;
+    }
+
+    public void setPostCategoryID(int postCategoryID) {
+        this.postCategoryID = postCategoryID;
+    }
+}
---
+++ newspaper/User.java
@@ -0,0 +1,94 @@
+package newspaper;
+
+public class User {
+    private int userID;
+    private String userFullName;
+    private String userLoginName;
+    private String userPassword;
+    private String userAddress;
+    private String userPhone;
+    private String userEmail;
+    private String userBirthDate;
+    private String userSex;
+    private String userRole;
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUserFullName() {
+        return userFullName;
+    }
+
+    public void setUserFullName(String userFullName) {
+        this.userFullName = userFullName;
+    }
+
+    public String getUserLoginName() {
+        return userLoginName;
+    }
+
+    public void setUserLoginName(String userLoginName) {
+        this.userLoginName = userLoginName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserBirthDate() {
+        return userBirthDate;
+    }
+
+    public void setUserBirthDate(String userBirthDate) {
+        this.userBirthDate = userBirthDate;
+    }
+
+    public String getUserSex() {
+        return userSex;
+    }
+
+    public void setUserSex(String userSex) {
+        this.userSex = userSex;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+}
